cadb59c50a741b1f850dcf5fcf72f879bd71af72
Kato Shinya <kato.shinya.dev@gmail.com>
2022-03-12 22:51:08 +0900

    applied` import_sorter`

--- lib/src/job/task/shutdown_task.dart
+++ lib/src/job/task/shutdown_task.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// Project imports:
 import 'package:batch/src/job/context/execution_context.dart';
 import 'package:batch/src/job/entity/task.dart';
 
