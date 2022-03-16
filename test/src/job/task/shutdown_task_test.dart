4b4465c2537d74c3a5a9d2bb6bfdabeb6b092fa6
myConsciousness <kato.shinya.dev@gmail.com>
2022-03-16 17:55:20 +0900

    fixed test

--- test/src/job/task/shutdown_task_test.dart
+++ test/src/job/task/shutdown_task_test.dart
@@ -10,11 +10,16 @@ import 'package:batch/src/batch_instance.dart';
 import 'package:batch/src/batch_status.dart';
 import 'package:batch/src/job/context/execution_context.dart';
 import 'package:batch/src/job/task/shutdown_task.dart';
+import 'package:batch/src/log/log_configuration.dart';
+import 'package:batch/src/log/logger.dart';
 
 void main() {
   test('Test ShutdownTask', () {
     BatchInstance.instance.updateStatus(BatchStatus.running);
     expect(BatchInstance.instance.isRunning, true);
+
+    //! Required to load logger to run Shutdown task.
+    Logger.loadFrom(config: LogConfiguration(printLog: false));
 
     final task = ShutdownTask();
     expect(() => task.execute(ExecutionContext()), returnsNormally);
