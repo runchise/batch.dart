a1f1a1e70db970756d4ab057af681459a6a83ac6
Kato Shinya <kato.shinya.dev@gmail.com>
2022-03-22 10:53:08 +0900

    feat: (#32)

--- lib/src/banner/banner_printer.dart
+++ lib/src/banner/banner_printer.dart
@@ -23,5 +23,5 @@ class _BannerPrinter implements BannerPrinter {
   final Banner banner;
 
   @override
-  Future<void> execute() async => info(await banner.build());
+  Future<void> execute() async => info('\n${await banner.build()}');
 }
