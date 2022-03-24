c5015a23998c98be73bf81a6181f0fa50a57467f
Kato Shinya <kato.shinya.dev@gmail.com>
2022-03-24 18:26:12 +0900

    Update update_notification.dart

--- lib/src/version/update_notification.dart
+++ lib/src/version/update_notification.dart
@@ -1,7 +1,6 @@
 // Copyright 2022 Kato Shinya. All rights reserved.
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided the conditions.
-// See more details at https://github.com/batch-dart/batch.dart/blob/main/LICENSE
 
 // Package imports:
 import 'package:meta/meta.dart';
