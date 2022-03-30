ac19afb096fdd2107b97e6460fa2487a740cc4ea
myConsciousness <kato.shinya.dev@gmail.com>
2022-03-30 16:09:12 +0900

    feat: fix for the issue (#74)

--- lib/src/job/parallel/parallel_executor.dart
+++ lib/src/job/parallel/parallel_executor.dart
@@ -11,6 +11,8 @@ import 'package:async_task/async_task.dart';
 // Project imports:
 import 'package:batch/src/job/context/execution_context.dart';
 import 'package:batch/src/job/task/parallel_task.dart';
+import 'package:batch/src/log/log_configuration.dart';
+import 'package:batch/src/log/logger.dart';
 
 class ParallelExecutor extends AsyncTask<String, void> {
   /// Returns the new instance of [ParallelExecutor].
@@ -18,6 +20,9 @@ class ParallelExecutor extends AsyncTask<String, void> {
 
   /// The parallel task
   final ParallelTask parallelTask;
+
+  /// The log configuration from main thread
+  LogConfiguration? logConfig;
 
   @override
   AsyncTask<String, void> instantiate(String parameters,
@@ -32,6 +37,9 @@ class ParallelExecutor extends AsyncTask<String, void> {
 
   @override
   FutureOr<void> run() async {
+    assert(logConfig != null);
+    Logger.loadFrom(config: logConfig!);
+
     try {
       await parallelTask.execute(ExecutionContext());
     } catch (e) {
